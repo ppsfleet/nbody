ee4e11fc69e139f3aa6117e970be39b7c9858e7a
kornakh <larkin@laposte.net>
2018-03-09 16:42:29 +0100

    [MAIN] parsing arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include "BruteForceSimulation.h"
 #include "BarnesHutSimulation.h"
 #include "Octree.h"
+#include <string>
+#include <boost/lexical_cast.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, const char *argv[])
+using namespace std;
+int main(int argc, char *argv[])
 {
+
+  // Default
+  int opt;
+  int nbIter = 500;
+  int intervIter = 1;
+  string algo = "bruteforce";
+
+  // parse argument
+  const char *optString = /*n*/":a:n:i:?";
+  while ((opt = getopt(argc,argv,optString)) != EOF)
+  switch(opt)
+  {
+      case 'a': algo = optarg ; break;
+      case 'n': nbIter = boost::lexical_cast<int>(optarg); break;
+      case 'i': intervIter = boost::lexical_cast<int>(optarg); break;
+      case '?': fprintf(stderr, "usuage is \n -a <value>: for determining used algorithm, [barneshut, bruteforce] default :  bruteforce \n"
+        " -n <value>: for setting the number of iteration, default : 500\n"
+        " -i <value>: for setting the time between each iteration, default : 1\n ");
+      default: cout<<endl; abort();
+  }
+
+  // treatment
+  cout << "algo " << algo << " nb itération " << nbIter << " tps Interval " << intervIter << endl;
+  if(algo == "barneshut"){
+    //BruteForceSimulation simulation;
+    //simulation.run(nbIter, intervIter);
+  }
+  else if(algo == "bruteforce"){
+    //BarnesHutSimulation simulation;
+    //simulation.run(nbIter, intervIter);
+  }else{
+    std::cerr <<"Unknown algorithm, try bruteforce or barneshut"<<endl;
+  }
 
   BruteForceSimulation simulation;
   simulation.run(2500);
