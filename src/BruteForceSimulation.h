f600878712a411a9fcc60dd783987b9df907f5b6
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2018-03-20 17:12:28 +0100

    start working on multithreading

--- src/BruteForceSimulation.h
+++ src/BruteForceSimulation.h
@@ -12,26 +12,26 @@ class BruteForceSimulation: public Simulation<Particle> {
   public:
     void step (double deltaT) {
       for (auto p: m_particles) {
-        p->setForce(0, 0, 0);
+        p->setForce(0.0, 0.0, 0.0);
       }
       for (unsigned int i = 0; i < m_particles.size(); i++) {
-        //std::cerr << m_particles[i]->m_id << " ";
-        for (unsigned int j = 0; j < m_particles.size(); j++) {
-          if (i != j) {
-            //std::cerr << m_particles[j]->m_id << ", ";
+        //double force[3] = {0};
+        #pragma omp parallel for
+        for (unsigned int j = i+1; j < m_particles.size(); j++) {
+          /*if(i != j) {
+            double loca_force[3] = {};
+            IParticle::computeForce(m_particles[i], m_particles[j], loca_force);
+
+            force[0] += loca_force[0];
+            force[1] += loca_force[1];
+            force[2] += loca_force[2];
             //Particle::computeForce(m_particles[i], m_particles[j]);
-            double force[3];
-            IParticle::computeForce(m_particles[i], m_particles[j], force);
-            m_particles[i]->addForce(force[0], force[1], force[2]);
-          }
+          }*/
+          Particle::computeForce(m_particles[i], m_particles[j]);
         }
-        //std::cerr << std::endl;
-        //m_particles[i]->update(deltaT);
-        //std::cout << *m_particles[i] << std::endl;
-      }
-      for (auto p: m_particles) {
-        p->update(deltaT);
-        std::cout << *p << std::endl;
+        //m_particles[i]->addForce(force[0], force[1], force[2]);
+        m_particles[i]->update(deltaT);
+        std::cout << *m_particles[i] << std::endl;
       }
     }
 };
